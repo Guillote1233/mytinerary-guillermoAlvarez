f9ddc7817a80bad20b6a0d95f24b3c66280fe289
Guillote1233 <guialvarez123@gmail.com>
2023-08-08 15:46:37 -0300

    polish

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>F</div>
+    <footer className='bg-indigo-600 text-white'>
+        const socialIcons = [
+
+        ];
+
+        <div className='md:flex md:justify-between md:items-center sm:px-12 px-4 py-7'>
+            <div>
+                
+            </div>
+
+        </div>
+    </footer>
   )
 }
 
