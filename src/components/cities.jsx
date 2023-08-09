d7382526046e05578a7f5d56afb4c4225012aaec
Guillote1233 <guialvarez123@gmail.com>
2023-08-09 15:43:57 -0300

    image for cities

--- src/components/cities.jsx
+++ src/components/cities.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cities(){
   return (
     <div className='flex justify-center items-center bg-indigo-400 h-[90.7vh]'>
-        <img src="/underconstruction.png" alt="coming soon" />
+        <img src='https://media.giphy.com/media/cfGmVRsJI6wq6noGxP/giphy.gif' className='w-96 h-96' />
     </div>
   )
 }
